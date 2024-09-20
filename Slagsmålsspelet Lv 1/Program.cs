d9c59f7205af94365a0aa6ca0c93ff00f85ce8d4
Anton Svedin <anton.svedin@elev.ga.ntig.se>
2024-09-20 10:55:12 +0200

    Fixat lite med koden

--- Slagsmålsspelet Lv 1/Program.cs
+++ Slagsmålsspelet Lv 1/Program.cs
@@ -1,12 +1,7 @@
-﻿while (true)
-{
-int card = Random.Shared.Next(10, 20);
-Console.WriteLine(card);
-Console.ReadLine();
-}
+﻿int Pl_A_HP = 100;
+int Pl_B_HP = 100;
 
-
-
+Console.WriteLine("Välkommen till mitt slagsmålsspel");
 Console.WriteLine("Skriv in ditt namn");
 
 string name  = Console.ReadLine();
@@ -21,3 +16,10 @@ while (name.Length < 3)
 Console.WriteLine($"Hej {name}");
 
 Console.ReadLine();
+
+while (true)
+{
+int card = Random.Shared.Next(10, 20);
+Console.WriteLine(card);
+Console.ReadLine();
+}
