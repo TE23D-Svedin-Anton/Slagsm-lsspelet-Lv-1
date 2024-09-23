c09cf3b7d5aa2de1c6877985b79690dd0a922b2b
Anton Svedin <anton.svedin@elev.ga.ntig.se>
2024-09-23 09:56:09 +0200

    Fixat så rundorna räknas och ska fixa en funktion som heter fight over när någon har förlorat

--- Slagsmålsspelet Lv 1/Program.cs
+++ Slagsmålsspelet Lv 1/Program.cs
@@ -1,24 +1,46 @@
 ﻿int Pl_A_HP = 100;
 int Pl_B_HP = 100;
+int Round = 0;
+bool RoundOver = true;
+bool FightOver = false;
+
+static int NästaRunda(bool RoundOver)
+{
+    if (RoundOver == true)
+    {
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
+}
 
 Console.WriteLine("Välkommen till mitt slagsmålsspel");
 Console.WriteLine("Skriv in ditt namn");
 
-string name  = Console.ReadLine();
+string name = Console.ReadLine();
 while (name.Length < 3 || !name.All(Char.IsLetter))
 {
     Console.WriteLine("Skriv in ditt namn, sa jag, dumhuvve");
     name = Console.ReadLine();
 }
 
-
 Console.WriteLine($"Hej {name}");
-
+Console.WriteLine("Tryck Enter För Att Börja");
 Console.ReadLine();
+
+Round += NästaRunda(RoundOver);
+
+if (FightOver == true)
+{
+    Console.WriteLine($"Runda Nummer {Round}");
+}
+
 
 while (true)
 {
-int card = Random.Shared.Next(10, 20);
-Console.WriteLine(card);
-Console.ReadLine();
+    int card = Random.Shared.Next(10, 20);
+    Console.WriteLine(card);
+    Console.ReadLine();
 }
