4c3435fcf5393377c735f0a359a67225adfae552
Anton Svedin <anton.svedin@elev.ga.ntig.se>
2024-09-27 10:51:30 +0200

    Håller på att fixa damage systemet men behöver göra lite mer ändringar så de inte uppkomer fel under tiden spelet är igång

--- Slagsmålsspelet Lv 1/Program.cs
+++ Slagsmålsspelet Lv 1/Program.cs
@@ -3,9 +3,8 @@ int Pl_2_HP = 100;
 
 string Pl2Name = "Dum Person";
 
-int Round = 0;
-bool RoundOver = true;
-bool FightOver = false;
+int Round = 1;
+bool RoundOver = false;
 
 static int NästaRunda(bool RoundOver, string Pl1Name, int Pl_1_HP, string Pl2Name, int Pl_2_HP)
 {
@@ -27,23 +26,28 @@ string Pl1Name = Console.ReadLine();
 while (Pl1Name.Length < 3 || !Pl1Name.All(Char.IsLetter))
 {
     Console.WriteLine("Skriv in ditt namn, sa jag, dumhuvve");
+    Pl1Name = Console.ReadLine();   
 }
 
 Console.WriteLine($"Hej {Pl1Name}");
 Console.WriteLine("Tryck Enter För Att Börja");
 Console.ReadLine();
 
-Round += NästaRunda(RoundOver, Pl1Name, Pl_1_HP, Pl2Name, Pl_2_HP);
-
-if (FightOver == true)
+while (Pl_1_HP > 0 &&  Pl_2_HP > 0)
 {
-    Console.WriteLine($"Runda Nummer {Round}");
+    if (RoundOver = true)
+{
+    Console.WriteLine($"\nRunda{Round}");
+
+    int Pl_1_Damage = Random.Shared.Next(20);
+    Pl_2_HP -= Pl_1_Damage;
+    Pl_2_HP = Math.Max(0, Pl_2_HP);
+
+    int Pl_2_Damage = Random.Shared.Next(20);
+    Pl_1_HP -= Pl_2_Damage;
+    Pl_1_HP = Math.Max(0, Pl_1_HP);
+    Round += NästaRunda(RoundOver, Pl1Name, Pl_1_HP, Pl2Name, Pl_2_HP);
+    Console.ReadKey();
+
 }
-
-
-while (true)
-{
-    int card = Random.Shared.Next(10, 20);
-    Console.WriteLine(card);
-    Console.ReadLine();
 }
